bcc54c1d1687d7f94f02fffa356b6e6e893f8cf4
hiroakni <hiroakni@gmail.com>
2018-11-04 15:42:18 +0900

    Update AppContext.cs

--- TodoApp_2/TodoApp/Models/AppContext.cs
+++ TodoApp_2/TodoApp/Models/AppContext.cs
@@ -4,11 +4,9 @@ namespace TodoApp.Models
 {
     public class AppContext : DbContext
     {
-        /*
         public AppContext() : base("name=TodoDbConnection")
         {
         }
-        */
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Todo> Todoes { get; set; }
