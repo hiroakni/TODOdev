201e5472dfc66f7f002807b23c175e220232738c
hiroakni <hiroakni@gmail.com>
2018-11-11 04:11:23 +0900

    Update AppContext.cs

--- TodoApp_2/TodoApp/Models/AppContext.cs
+++ TodoApp_2/TodoApp/Models/AppContext.cs
@@ -4,11 +4,10 @@ namespace TodoApp.Models
 {
     public class AppContext : DbContext
     {
-        /*
+        // Azure上のDBに接続しに行く処理。ローカルで開発する場合はここをコメントアウト
         public AppContext() : base("name=TodoDbConnection")
         {
         }
-        */
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Todo> Todoes { get; set; }
